a70e6b7185480587286aab921836bcfcd7a2c156
Shoyeb <heroreal5385@gmail.com>
2021-11-28 12:15:22 +0600

    boss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from "./Chat"
+
+
 function App() {
   return (
     <div className="app">
       <div className='app-body'>
         <Sidebar />
+        <Chat />
         </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyCbic6TisGckPCHq32vnFN32cyAyDgsTCw",
     authDomain: "whatsapp-2b090.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-a468f.firebase",
     projectId: "whatsapp-2b090",
     storageBucket: "whatsapp-2b090.appspot.com",
     messagingSenderId: "1022814377604",
     appId: "1:1022814377604:web:3eba8c90f436de112e14d1",
     measurementId: "G-2090BGL1KF"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth=firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth,provider};
+export default db;
